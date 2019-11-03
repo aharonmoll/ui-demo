4546aaf0f22241838dc849edda7025d22c4c765b
yohanakh <yohana.khoury@gmail.com>
2019-11-03 15:40:29 +0200

    uncomment lines in deploy_all

--- deploy_all.sh
+++ deploy_all.sh
@@ -16,13 +16,13 @@ if [[ -n "${SERVER}" ]]; then
 fi
 
 
-#${GS_DIR}/bin/gs.sh pu deploy Mirror ${DIRNAME}/mirror/target/mirror.jar
+${GS_DIR}/bin/gs.sh pu deploy Mirror ${DIRNAME}/mirror/target/mirror.jar
 
-#${GS_DIR}/bin/gs.sh pu deploy --partitions=2 --backups=1 ProductsCatalog ${DIRNAME}/products-catalog/target/products-catalog.jar
+${GS_DIR}/bin/gs.sh pu deploy --partitions=2 --backups=1 ProductsCatalog ${DIRNAME}/products-catalog/target/products-catalog.jar
 
-#${GS_DIR}/bin/gs.sh pu deploy ProductsLoader ${DIRNAME}/products-loader/target/products-loader.jar
+${GS_DIR}/bin/gs.sh pu deploy ProductsLoader ${DIRNAME}/products-loader/target/products-loader.jar
 
 #${GS_DIR}/bin/gs.sh pu deploy ProductsFeeder ${DIRNAME}/products-feeder/target/products-feeder.jar
 
-${GS_DIR}/bin/gs.sh pu deploy --partitions=2 WebApplication ${DIRNAME}/web-application/target/web-application.war
+${GS_DIR}/bin/gs.sh pu deploy --instances=2 WebApplication ${DIRNAME}/web-application/target/web-application.war
 
