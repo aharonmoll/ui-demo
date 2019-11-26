e6cbf27ac79931fe086ab97c0f3abcc4e3c36d50
Efrat Kimchi <efrat.kimchi@gigaspaces.com>
2019-11-26 16:10:53 +0200

    memory alert -during prevent memory shortage exception

--- demo-app/src/main/java/com/gigaspaces/ui/demo/controllers/AppMain.java
+++ demo-app/src/main/java/com/gigaspaces/ui/demo/controllers/AppMain.java
@@ -24,7 +24,7 @@ public class AppMain {
         }*/
 
         try {
-            System.out.println(controller.triggerMemoryAlertOnService("ProductsCatalog", 60));
+            System.out.println(controller.triggerMemoryAlertOnService("ProductsCatalog", 90));
         } catch (ApiException e) {
             e.printStackTrace();
         }
